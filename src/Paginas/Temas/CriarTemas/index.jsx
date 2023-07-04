6bc2d16886cdbf94c3d40307ccd4cf855c25d8e8
davi-canuto <davicanutogregorio@gmail.com>
2023-07-04 20:36:16 -0300

    feat: improvement list and create themes

---
+++ src/Paginas/Temas/CriarTemas/index.jsx
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+
+import InputTexto from '../../../Componentes/Inputs/InputTexto.jsx';
+import { useState, useEffect } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../../Temas.jsx';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
+import Botao from '../../../Componentes/Botao/Botao.jsx';
+import api from '../../../services/api.jsx';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+export default function MyApp() {
+  const [nome, setName] = useState('');
+  const [cor, setCor] = useState('');
+  const [price, setPrice] = useState('');
+  const [itens, setItens] = useState([]);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const props = location.state ? location.state : null;
+
+  const routeChangeBack = () => {
+    let path = `/temas`;
+    navigate(path);
+  }
+
+  const handleNome = (event) => {
+    setName(event);
+  };
+
+  const handleCor = (event) => {
+    setCor(event);
+  };
+
+  const handlePrice = (event) => {
+    setPrice(event);
+  };
+
+  const handleItens = (event) => {
+    setItens(event.target.value);
+  };
+
+  async function submit() {
+    if (nome === '') {
+      console.log('não pode');
+      return;
+    }
+    try {
+      const data = {
+        "name": nome,
+        "color": cor,
+        "price": price,
+        "itens": itens.map(item => item.id)
+      };
+      const response = props ? api.put(`themes/${props.id}/`, data) : await api.post('themes/', data);
+      routeChangeBack();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchItens() {
+      try {
+        const response = await api.get('itens/');
+        setItens(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchItens();
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography variant="h3">{props ? "Edição" : "Cadastro"} de Tema</Typography>
+      <div>
+        <InputTexto
+          label='Nome'
+          value={nome}
+          onChange={handleNome}
+        />
+        <InputTexto
+          label='Cor'
+          value={cor}
+          onChange={handleCor}
+        />
+        <InputTexto
+          label='Preço'
+          value={price}
+          onChange={handlePrice}
+        />
+        <Box display="flex" justifyContent="space-between">
+            <FormControl sx={{ m: 1, width: 300 }}>
+            <InputLabel id="demo-multiple-checkbox-label">Itens</InputLabel>
+            <Select
+                labelId="demo-multiple-checkbox-label"
+                id="demo-multiple-checkbox"
+                multiple
+                value={itens}
+                onChange={handleItens}
+                input={<OutlinedInput label="Itens" />}
+                renderValue={(selected) => selected.map(item => item.name).join(', ')}
+                MenuProps={MenuProps}
+            >
+                {itens.map((item) => (
+                <MenuItem key={item.id} value={item}>
+                    <Checkbox checked={itens.some(selectedItem => selectedItem.id === item.id)} />
+                    <ListItemText primary={item.name} />
+                </MenuItem>
+                ))}
+            </Select>
+            </FormControl>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Botao variant="outlined" onClick={routeChangeBack} label="Voltar" />
+          <Botao variant="contained" onClick={submit} label={props ? "Editar" : "Criar"} />
+        </Box>
+      </div>
+    </ThemeProvider>
+  );
+}
--- src/Paginas/Temas/ListarTemas/index.jsx
+++ src/Paginas/Temas/ListarTemas/index.jsx
@@ -1,13 +1,104 @@
-import Typography from '@mui/material/Typography';
-import theme from '../../../Temas.jsx'
-import { ThemeProvider } from '@mui/material/styles';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../../../Temas.jsx";
+import Typography from "@mui/material/Typography";
+import { Box, Grid, Container, TextField } from "@mui/material";
+import Botao from "../../../Componentes/Botao/Botao.jsx";
+import api from "../../../services/api.jsx";
+import './index.css'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const index = () => {
+export default function Index() {
+  const [temas, setTemas] = useState([]);
+  const [filtro, setFiltro] = useState('');
+  const navigate = useNavigate();
+
+  const filtrarDados = (event) => {
+    setFiltro(event.target.value);
+  };
+
+  const mudarRota = () => {
+     navigate("/temas/criar");
+  };
+
+  const editarTema = (id) => {
+    const props = {
+      id: id
+    };
+    navigate('/temas/editar', { state: props });
+ };
+
+  async function deletarTema (id){
+    try {
+      const response = await api.delete(`temas/${id}/`);
+      const temas = await api.get("temas/");
+      setTemas(temas.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  const dadosFiltrados = temas.filter((tema) => {
+      if (tema.name && tema.name.toLowerCase().includes(filtro.toLowerCase())){
+        return tema
+      }
+    }
+
+  );
+
+  useEffect(() => {
+    async function fetchtemas() {
+      try {
+        const response = await api.get("themes/");
+        setTemas(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchtemas();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-        <Typography variant="h3">Falta implementar - Temas</Typography>
-    </ThemeProvider>
-  )
-}
+      <Container sx={{ width: '900px' }}>
+        <Typography mb={2} variant="h3">Temas</Typography>
+        <Grid container justifyContent={'space-between'} ml={1.8} p={1}>
+          <TextField
+            label="Buscar"
+            value={filtro}
+            onChange={filtrarDados}
+            variant="outlined"
+            sx={{
+              width: '50%'
+            }}
+          />
+          <Box mr={3}><Botao variant="contained" onClick={mudarRota} label="Novo Tema" /></Box>
 
-export default index;
+        </Grid>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: 500,
+            overflow: "hidden",
+            overflowY: "scroll"
+          }}>
+          {dadosFiltrados.map((elemento) => (
+            <Box display="flex" alignItems="center" justifyContent={'space-between'} boxShadow={3} key={elemento.id} m={1.3} p={1} sx={{ backgroundColor: "#f8eeee", borderRadius: 1, height: '38px' }}>
+              <Typography ml={2} variant="subtitle1">{elemento.name}</Typography>
+              <Typography ml={2} variant="subtitle1">{elemento.price}</Typography>
+              <Box mt={0.6}>
+                <EditIcon onClick={() => editarTema(elemento.id)} style={{ color: '#3B1D70', marginRight:'10px', cursor: 'pointer' }} />
+                <DeleteIcon onClick={() => deletarTema(elemento.id)} style={{ color: 'red', cursor: 'pointer' }} />
+              </Box>
+            </Box>
+          ))}
+        </Box>
+
+      </Container>
+    </ThemeProvider>
+  );
+}
